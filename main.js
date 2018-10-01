244af8f9561bf7d8c1472379e205d2c3f86d955d
Sean McQuaid <sean.mcquaid2@verizon.net>
2018-10-01 15:00:41 -0400

    sketched out the logic for the rest of the project

--- main.js
+++ main.js
@@ -1,9 +1,32 @@
 // declare variable for name that is undefined
 let name;
 
+// create array of images
+
+const images = [];
+
 // add on click listener that will save name from input then generate alert
 
 document.querySelector('#name-button').addEventListener("click", function(){
   name = document.querySelector('.player').value;
   alert("Run " + name + " RUN!!!");
 }); 
+
+// generate random image 
+function randomImages (){
+ // make equation for random images
+ // set new image equal to random image in array
+ // return the image
+};
+
+// create click listener on each card image that generates the new random image
+ document.querySelectorAll('.flash-card').addEventListener("click", function(){
+
+ });
+// if image is iris, give the player 5 points
+
+// if image isn't iris, give computer 5 points
+
+// if score for either player hits 15, games ends with text indicating winner - find the winner via for loop that will return text
+
+//click listener that resets the game
